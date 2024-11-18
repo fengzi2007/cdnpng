6fedc5e65b36bd5444a8a06245c84b24f1100418
fengzi2007 <87165482+fengzi2007@users.noreply.github.com>
2024-11-18 22:08:28 +0800

    Update js-cdn-txt.js

--- js-cdn-txt.js
+++ js-cdn-txt.js
@@ -2,7 +2,6 @@
 const htmlSnippets = [
     { html: '<a target="_blank" href="https://www.ydbee.cn/" title="云端蜜蜂，为您免费提供网站加速和网站防御（DDOS、CC攻击）" alt="云端蜜蜂，为您免费提供网站加速和网站防御（DDOS、CC攻击）">本站由云端蜜蜂提供网站加速和攻击防御服务</a>', weight: 3 },
     { html: '<a target="_blank" href="https://www.fusionidc.cn/" title="聚变云，为您免费提供网站加速和网站防御（DDOS、CC攻击）" alt="聚变云，为您免费提供网站加速和网站防御（DDOS、CC攻击）">本站由聚变云数据提供网站加速和攻击防御服务</a>', weight: 1 },
-    { html: '<a target="_blank" href="https://idc.7msb.com/" title="花卷IDC，为您免费提供网站加速和网站防御（DDOS、CC攻击）" alt="花卷IDC，为您免费提供网站加速和网站防御（DDOS、CC攻击）">本站由花卷IDC提供网站加速和攻击防御服务</a>', weight: 1 }
 ];
 
 // 计算总权重
